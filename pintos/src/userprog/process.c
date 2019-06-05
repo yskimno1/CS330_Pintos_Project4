e358cd1659d417c16a277787fd6bf9a3d8e49620
윤성 <yskimno1@gmail.com>
2019-06-05 21:00:30 +0900

    at process exit

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -176,6 +176,8 @@ process_exit (void)
       }
     }
 	}
+  
+  if(thread_current()->current_dir != NULL) dir_close(thread_current()->current_dir);
 
   sema_up(&curr->sema_wait);
   /* wait until parent removes the child in the list */
