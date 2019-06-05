502a01ea54461a1b9207d42db6ee9c49252038ef
윤성 <yskimno1@gmail.com>
2019-06-05 20:49:57 +0900

    typo

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -19,6 +19,7 @@ enum thread_status
 /* Thread identifier type.
    You can redefine this to whatever type you like. */
 typedef int tid_t;
+
 #define TID_ERROR ((tid_t) -1)          /* Error value for tid_t. */
 /* Thread priorities. */
 #define PRI_MIN 0                       /* Lowest priority. */
