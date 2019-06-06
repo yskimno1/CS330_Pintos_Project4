4db02ad14ec315cfc40af2833bedd860307004e8
윤성 <yskimno1@gmail.com>
2019-06-06 19:49:38 +0900

    test

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -170,7 +170,7 @@ filesys_create (const char *name, off_t initial_size, bool is_dir)
                   && free_map_allocate (1, &inode_sector)
                   && inode_create (inode_sector, initial_size, is_dir)
                   && dir_add (dir, filename, inode_sector));
-
+  
   if (!success && inode_sector != 0) 
     free_map_release (inode_sector, 1);
 
