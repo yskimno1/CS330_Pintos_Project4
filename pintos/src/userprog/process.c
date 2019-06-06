bd7323c442b1203b45225b2d1c3752ad7aff23af
윤성 <yskimno1@gmail.com>
2019-06-07 00:44:19 +0900

    changed location of file deny write

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -354,6 +354,8 @@ load (const char *file_name, void (**eip) (void), void **esp)
       goto done; 
     }
 
+  file_deny_write(file);
+  
   /* Read and verify executable header. */
   if (file_read (file, &ehdr, sizeof ehdr) != sizeof ehdr
       || memcmp (ehdr.e_ident, "\177ELF\1\1\1", 7)
@@ -439,7 +441,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
   *eip = (void (*) (void)) ehdr.e_entry;
 
   success = true;
-  file_deny_write(file);
+
  done:
   /* We arrive here whether the load is successful or not. */
   
