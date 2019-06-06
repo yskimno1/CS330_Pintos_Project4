1662c553d9999e22688827831d21d6340872a06a
윤성 <yskimno1@gmail.com>
2019-06-06 22:49:34 +0900

    testing

--- pintos/src/filesys/file.c
+++ pintos/src/filesys/file.c
@@ -94,7 +94,9 @@ file_read_at (struct file *file, void *buffer, off_t size, off_t file_ofs)
 off_t
 file_write (struct file *file, const void *buffer, off_t size) 
 {
+  printf("file_write, inode : %p\n", file->inode);
   off_t bytes_written = inode_write_at (file->inode, buffer, size, file->pos);
+  printf("bytes_written : %d\n", bytes_written);
   file->pos += bytes_written;
   return bytes_written;
 }
