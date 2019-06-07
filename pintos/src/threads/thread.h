25d1546a2e7fd141a01c1758d77e36d55bc06c09
윤성 <yskimno1@gmail.com>
2019-06-07 13:56:29 +0900

    check filemax

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -83,7 +83,7 @@ typedef int tid_t;
    ready state is on the run queue, whereas only a thread in the
    blocked state is on a semaphore wait list. */
 
-#define FILE_MAX 160
+#define FILE_MAX 131
 struct thread
   {
     /* Owned by thread.c. */
